1b17ee768a2f84291123f35a11523c889f9a4014
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-10-10 11:13:01 -0700

    [Feat] Sync when session completes (#17)
    * sync when session completes
    
    * pretty

--- src/hooks/useSyncInterval.ts
+++ src/hooks/useSyncInterval.ts
@@ -5,19 +5,37 @@ import { useEffect } from 'react'
 export function useSyncPlaySession(
   projectId: string,
   invalidateQuery: () => Promise<void>,
-  syncPlaySession: (appName: string, runner: Runner) => Promise<void>
+  syncPlaySession: (appName: string, runner: Runner) => Promise<void>,
+  minimumRequiredPlayTimeInSeconds?: number,
+  currentPlayTimeInSeconds?: number
 ) {
   useEffect(() => {
-    const syncTimer = setInterval(async () => {
+    async function sync() {
       await syncPlaySession(projectId, 'hyperplay')
       // allow for some time before read
       await wait(5000)
       await invalidateQuery()
       resetSessionStartedTime()
-    }, 1000 * 60)
+    }
+    const syncTimer = setInterval(sync, 1000 * 60)
+
+    let finalSyncTimer: NodeJS.Timeout | undefined = undefined
+    if (
+      minimumRequiredPlayTimeInSeconds &&
+      currentPlayTimeInSeconds &&
+      currentPlayTimeInSeconds < minimumRequiredPlayTimeInSeconds
+    ) {
+      finalSyncTimer = setTimeout(
+        sync,
+        minimumRequiredPlayTimeInSeconds - currentPlayTimeInSeconds
+      )
+    }
 
     return () => {
       clearInterval(syncTimer)
+      if (finalSyncTimer) {
+        clearTimeout(finalSyncTimer)
+      }
     }
   }, [projectId, invalidateQuery])
 }
