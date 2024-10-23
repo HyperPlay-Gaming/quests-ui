096d4a1fb6d5d12778ce9a38c59d0b45f73e912c
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-10-22 23:08:49 -0700

    [Feat] Add get play streak quest status (#26)
    * ad get playstreak quest status
    
    * add jest
    
    * add tests for get playstreak quest status
    
    * export get playstreak quest status
    
    * export use get quest
    
    * bump

---
+++ src/helpers/getPlaystreakQuestStatus.test.ts
@@ -0,0 +1,120 @@
+import { Quest, UserPlayStreak } from '@hyperplay/utils'
+import { getPlaystreakQuestStatus } from './getPlaystreakQuestStatus'
+
+describe('get playstreak quest status test', () => {
+  test('Get active status since quest is infinitely repeatable', () => {
+    const quest: Quest = {
+      id: 0,
+      project_id: '0123',
+      name: 'a quest',
+      type: 'PLAYSTREAK',
+      status: 'ACTIVE',
+      description: 'description',
+      deposit_contracts: [],
+      quest_external_game: null,
+      num_of_times_repeatable: null,
+      eligibility: {
+        steam_games: [],
+        play_streak: {
+          required_playstreak_in_days: 10,
+          minimum_session_time_in_seconds: 100
+        }
+      }
+    }
+    const playstreak: UserPlayStreak = {
+      current_playstreak_in_days: 0,
+      completed_counter: 1,
+      accumulated_playtime_today_in_seconds: 100,
+      last_play_session_completed_datetime: '0'
+    }
+    expect(getPlaystreakQuestStatus(quest, playstreak)).toEqual('ACTIVE')
+  })
+
+  test('Get ready for claim status', () => {
+    const quest: Quest = {
+      id: 0,
+      project_id: '0123',
+      name: 'a quest',
+      type: 'PLAYSTREAK',
+      status: 'ACTIVE',
+      description: 'description',
+      deposit_contracts: [],
+      quest_external_game: null,
+      num_of_times_repeatable: null,
+      eligibility: {
+        steam_games: [],
+        play_streak: {
+          required_playstreak_in_days: 10,
+          minimum_session_time_in_seconds: 100
+        }
+      }
+    }
+    const playstreak: UserPlayStreak = {
+      current_playstreak_in_days: 100,
+      completed_counter: 1,
+      accumulated_playtime_today_in_seconds: 100,
+      last_play_session_completed_datetime: '0'
+    }
+    expect(getPlaystreakQuestStatus(quest, playstreak)).toEqual(
+      'READY_FOR_CLAIM'
+    )
+  })
+
+  test('Get claimed status', () => {
+    const quest: Quest = {
+      id: 0,
+      project_id: '0123',
+      name: 'a quest',
+      type: 'PLAYSTREAK',
+      status: 'ACTIVE',
+      description: 'description',
+      deposit_contracts: [],
+      quest_external_game: null,
+      num_of_times_repeatable: 1,
+      eligibility: {
+        steam_games: [],
+        play_streak: {
+          required_playstreak_in_days: 10,
+          minimum_session_time_in_seconds: 100
+        }
+      }
+    }
+    const playstreak: UserPlayStreak = {
+      current_playstreak_in_days: 100,
+      completed_counter: 1,
+      accumulated_playtime_today_in_seconds: 100,
+      last_play_session_completed_datetime: '0'
+    }
+    expect(getPlaystreakQuestStatus(quest, playstreak)).toEqual('CLAIMED')
+  })
+
+  test('Get active status for finite completeable quest', () => {
+    const quest: Quest = {
+      id: 0,
+      project_id: '0123',
+      name: 'a quest',
+      type: 'PLAYSTREAK',
+      status: 'ACTIVE',
+      description: 'description',
+      deposit_contracts: [],
+      quest_external_game: null,
+      num_of_times_repeatable: 1,
+      eligibility: {
+        steam_games: [],
+        play_streak: {
+          required_playstreak_in_days: 10,
+          minimum_session_time_in_seconds: 100
+        }
+      }
+    }
+    const playstreak: UserPlayStreak = {
+      current_playstreak_in_days: 1,
+      completed_counter: 0,
+      accumulated_playtime_today_in_seconds: 100,
+      last_play_session_completed_datetime: '0'
+    }
+    expect(getPlaystreakQuestStatus(quest, playstreak)).toEqual('ACTIVE')
+  })
+})
+
+export {}
---
+++ src/helpers/getPlaystreakQuestStatus.ts
@@ -0,0 +1,33 @@
+import { QuestLogInfo } from '@hyperplay/ui'
+import { Quest, UserPlayStreak } from '@hyperplay/utils'
+
+export function getPlaystreakQuestStatus(
+  quest: Quest,
+  questPlayStreak: UserPlayStreak
+): QuestLogInfo['state'] {
+  const completedCounter = questPlayStreak.completed_counter
+  const numTimesCompleteable = quest.num_of_times_repeatable
+
+  const currentPlaystreak = questPlayStreak.current_playstreak_in_days
+  if (quest.eligibility?.play_streak === undefined) {
+    throw 'no playstreak eligibility criteria provided. cannot get quest status'
+  }
+  const requiredPlaystreak =
+    quest.eligibility.play_streak.required_playstreak_in_days
+
+  const gameIsInfinitelyCompleteable = numTimesCompleteable === null
+  const gameHasMoreFiniteCompletionsPossible =
+    completedCounter < (numTimesCompleteable ?? Number.MAX_VALUE)
+  if (
+    currentPlaystreak >= requiredPlaystreak &&
+    (gameIsInfinitelyCompleteable || gameHasMoreFiniteCompletionsPossible)
+  ) {
+    return 'READY_FOR_CLAIM'
+  } else if (gameIsInfinitelyCompleteable) {
+    return 'ACTIVE'
+  } else if (!gameHasMoreFiniteCompletionsPossible) {
+    return 'CLAIMED'
+  }
+
+  return 'ACTIVE'
+}
