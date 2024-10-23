277a8949e1ea455ccb79ddb2f66ec2c6f92fb2ae
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-10-22 23:03:12 -0700

    add tests for get playstreak quest status

--- src/helpers/getPlaystreakQuestStatus.ts
+++ src/helpers/getPlaystreakQuestStatus.ts
@@ -9,18 +9,25 @@ export function getPlaystreakQuestStatus(
   const numTimesCompleteable = quest.num_of_times_repeatable
 
   const currentPlaystreak = questPlayStreak.current_playstreak_in_days
+  if (quest.eligibility?.play_streak === undefined) {
+    throw 'no playstreak eligibility criteria provided. cannot get quest status'
+  }
   const requiredPlaystreak =
-    quest.eligibility?.play_streak.required_playstreak_in_days
+    quest.eligibility.play_streak.required_playstreak_in_days
 
+  const gameIsInfinitelyCompleteable = numTimesCompleteable === null
+  const gameHasMoreFiniteCompletionsPossible =
+    completedCounter < (numTimesCompleteable ?? Number.MAX_VALUE)
   if (
-    requiredPlaystreak !== undefined &&
-    currentPlaystreak >= requiredPlaystreak
+    currentPlaystreak >= requiredPlaystreak &&
+    (gameIsInfinitelyCompleteable || gameHasMoreFiniteCompletionsPossible)
   ) {
     return 'READY_FOR_CLAIM'
-  } else if (numTimesCompleteable === null) {
+  } else if (gameIsInfinitelyCompleteable) {
     return 'ACTIVE'
-  } else if (completedCounter >= numTimesCompleteable) {
+  } else if (!gameHasMoreFiniteCompletionsPossible) {
     return 'CLAIMED'
   }
+
   return 'ACTIVE'
 }
