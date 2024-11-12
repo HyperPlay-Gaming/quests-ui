03ec74421bebbb1685698a9c98bb42cb112eea53
Elio Briceño <eliobricenov@gmail.com>
2024-11-12 19:40:02 -0400

    feat(quest-claim): add timeout

--- src/state/ClaimedRewardToastState.ts
+++ src/state/ClaimedRewardToastState.ts
@@ -1,15 +1,30 @@
 import { Reward } from '@hyperplay/utils'
 import { makeAutoObservable } from 'mobx'
 
+const CLEAR_TIMEOUT_MS = 5000
+
 class ClaimedRewardToastState {
   private reward: Reward | null = null
+  private clearTimeoutId: NodeJS.Timeout | undefined = undefined
 
   constructor() {
     makeAutoObservable(this)
   }
 
+  private startClearTimeout() {
+    if (this.clearTimeoutId) {
+      clearTimeout(this.clearTimeoutId)
+    }
+    
+    this.clearTimeoutId = setTimeout(() => {
+      this.reward = null
+      this.clearTimeoutId = undefined
+    }, CLEAR_TIMEOUT_MS)
+  }
+
   showClaimedReward(reward: Reward) {
     this.reward = reward
+    this.startClearTimeout()
   }
 
   get claimedReward() {
@@ -17,7 +32,11 @@ class ClaimedRewardToastState {
   }
 
   clearReward() {
+    if (this.clearTimeoutId) {
+      clearTimeout(this.clearTimeoutId)
+    }
     this.reward = null
+    this.clearTimeoutId = undefined
   }
 }
 
