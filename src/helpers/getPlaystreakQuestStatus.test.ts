22d5f74678dca1bfb94a87a08e957f1e28150c5a
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-12-06 00:23:52 -0800

    update getPlaystreakQuestStatus to hide if ineligible completed quest

--- src/helpers/getPlaystreakQuestStatus.test.ts
+++ src/helpers/getPlaystreakQuestStatus.test.ts
@@ -115,6 +115,63 @@ describe('get playstreak quest status test', () => {
     }
     expect(getPlaystreakQuestStatus(quest, playstreak)).toEqual('ACTIVE')
   })
+
+  test('Quest status is undefined for COMPLETED status quest with min playstreak not met', () => {
+    const quest: Quest = {
+      id: 0,
+      project_id: '0123',
+      name: 'a quest',
+      type: 'PLAYSTREAK',
+      status: 'COMPLETED',
+      description: 'description',
+      deposit_contracts: [],
+      quest_external_game: null,
+      num_of_times_repeatable: 1,
+      eligibility: {
+        steam_games: [],
+        play_streak: {
+          required_playstreak_in_days: 10,
+          minimum_session_time_in_seconds: 100
+        }
+      }
+    }
+    const playstreak: UserPlayStreak = {
+      current_playstreak_in_days: 1,
+      completed_counter: 0,
+      accumulated_playtime_today_in_seconds: 100,
+      last_play_session_completed_datetime: '0'
+    }
+    expect(getPlaystreakQuestStatus(quest, playstreak)).toEqual(undefined)
+  })
+
+  test('Quest status is READY_FOR_CLAIM for COMPLETED status quest with min playstreak met', () => {
+    const quest: Quest = {
+      id: 0,
+      project_id: '0123',
+      name: 'a quest',
+      type: 'PLAYSTREAK',
+      status: 'COMPLETED',
+      description: 'description',
+      deposit_contracts: [],
+      quest_external_game: null,
+      num_of_times_repeatable: 1,
+      eligibility: {
+        steam_games: [],
+        play_streak: {
+          required_playstreak_in_days: 10,
+          minimum_session_time_in_seconds: 100
+        }
+      }
+    }
+    const playstreak: UserPlayStreak = {
+      current_playstreak_in_days: 10,
+      completed_counter: 0,
+      accumulated_playtime_today_in_seconds: 100,
+      last_play_session_completed_datetime: '0'
+    }
+    expect(getPlaystreakQuestStatus(quest, playstreak)).toEqual('READY_FOR_CLAIM')
+  })
+
 })
 
 export {}
