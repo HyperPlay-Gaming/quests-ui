340b928454af3cd8e75a032c743b9e2bc2aca8e5
Elio Briceño <eliobricenov@gmail.com>
2025-04-14 17:15:02 -0400

    feat(external-eligibility): add translation support for leaderboard and view text

--- src/components/ExternalEligibility/index.tsx
+++ src/components/ExternalEligibility/index.tsx
@@ -1,22 +1,24 @@
 import { IconArrowUpRight } from '@tabler/icons-react'
 import styles from './index.module.scss'
 import classNames from 'classnames'
+import { useTranslation } from 'react-i18next'
 
 export function ExternalEligibility({
   externalLink
 }: {
   externalLink: string
 }) {
+  const { t } = useTranslation()
   return (
     <div className={styles.root}>
-      Leaderboard
+      {t('quest.externalEligibility.leaderboard', 'Leaderboard')}
       <a
         href={externalLink}
         target="_blank"
         rel="noopener noreferrer"
         className={classNames(styles.link, 'button-sm', 'weight--bold')}
       >
-        View
+        {t('quest.externalEligibility.view', 'View')}
         <IconArrowUpRight className={styles.icon} />
       </a>
     </div>
