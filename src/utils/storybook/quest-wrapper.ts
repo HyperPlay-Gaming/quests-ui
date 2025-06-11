76588656d49e96919da6deeae6dfa2bfcd84f0ab
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2025-06-10 17:37:44 -0700

    pretty

--- src/utils/storybook/quest-wrapper.ts
+++ src/utils/storybook/quest-wrapper.ts
@@ -32,8 +32,10 @@ export async function waitForAllCTAsToBeEnabled(canvasElement: HTMLElement) {
   const canvas = within(canvasElement)
   // await for the claim button to be enabled
   await waitFor(() => {
-    canvas.getAllByRole('button', { name: /Connect|Claim/i }).forEach((button) => {
-      expect(button).toBeEnabled()
-    })
+    canvas
+      .getAllByRole('button', { name: /Connect|Claim/i })
+      .forEach((button) => {
+        expect(button).toBeEnabled()
+      })
   })
 }
